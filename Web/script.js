b13b84f4fa87556c30be2f46f40ea061c9183ac1
rishav-jha-mech <rishav.rj74@gmail.com>
2022-12-27 22:07:48 +0530

    Link fixed

--- Web/script.js
+++ Web/script.js
@@ -2,7 +2,7 @@ const SastaReact = document.getElementById('main')
 downloadPDF = document.createElement('div');
 downloadPDF.setAttribute('class','item');
 downloadPDF.innerHTML = `
-<a href="https://github.com/rishav-jha-mech/NITDGP-results-ranked-sem4/raw/main/UG-5th.pdf" rel="noopener noreferrer"> <i class="fas bg-danger fa-file-pdf text-white fs-2"></i> Download PDF</a>
+<a href="https://github.com/rishav-jha-mech/NITDGP-results-ranked-sem5/raw/main/UG-5th.pdf" rel="noopener noreferrer"> <i class="fas bg-danger fa-file-pdf text-white fs-2"></i> Download PDF</a>
 `
 SastaReact.appendChild(downloadPDF)
 
